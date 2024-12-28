3a7953ddc805991c0120872616c141bb0edab683
Kor Dwarshuis <proximus@fastmail.com>
2024-12-28 20:02:24 +0100

    New

---
+++ scripts/modules/makeBackupOfWikiFiles.mjs
@@ -0,0 +1,38 @@
+import fs from 'fs';
+import path from 'path';
+
+
+// Create function that copies files from one directory to another
+/**
+ * Copies files from one directory to another.
+ * @param {string} sourceDir - The source directory.
+ * @param {string} targetDir - The target directory.
+ * @returns {Promise<void>}
+ */
+async function copyFiles(sourceDir, targetDir) {
+    try {
+        // Check if targetDir exists, if it exists, empty it
+        if (fs.existsSync(targetDir)) {
+            const files = await fs.promises.readdir(targetDir);
+            const deletePromises = files.map(file => fs.promises.unlink(path.join(targetDir, file)));
+            await Promise.all(deletePromises);
+        } else {
+            // If targetDir does not exist, create it
+            await fs.promises.mkdir(targetDir, { recursive: true });
+        }
+
+        const files = await fs.promises.readdir(sourceDir);
+        const promises = files
+            .filter(file => file !== '.git' && file !== '.gitignore')
+            .map(async (file) => {
+                const sourcePath = path.join(sourceDir, file);
+                const targetPath = path.join(targetDir, file);
+                await fs.promises.copyFile(sourcePath, targetPath);
+            });
+        await Promise.all(promises);
+    } catch (err) {
+        console.error(`Error copying files: ${err}`);
+        throw err;
+    }
+}
+export default copyFiles;
